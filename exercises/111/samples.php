2c86deb5da728736242c77feba5098a706d4e8c5
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-02-23 20:10:09 -0300

    Is Lucky...

---
+++ exercises/111/samples.php
@@ -0,0 +1,22 @@
+<?php
+
+class Samples
+{
+    public static function getSamples()
+    {
+        $samples = [
+            1230,
+            239017,
+            134008,
+            10,
+            11,
+            1010,
+            261534,
+            100000,
+            999999,
+            123321,
+        ];
+
+        return $samples;
+    }
+}
---
+++ exercises/111/solution.php
@@ -0,0 +1,15 @@
+<?php
+
+class Solution
+{
+    public function run($x)
+    {
+        return $this->isLucky($x);
+    }
+
+    private function isLucky($n)
+    {
+        return array_sum(str_split(substr($n, 0, strlen($n) / 2)))
+            == array_sum(str_split(substr($n, strlen($n) / 2)));
+    }
+}
