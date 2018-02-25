86247a6053f9d42fd24c165d38604200695cd2cc
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-02-24 21:59:02 -0300

    Is Lucky...

--- exercises/111/solution.php
+++ exercises/111/solution.php
@@ -4,7 +4,7 @@ class Solution
 {
     public function run($x)
     {
-        return $this->isLucky($x);
+        return $this->isLuckyV2($x);
     }
 
     private function isLucky($n)
@@ -15,10 +15,10 @@ class Solution
 
     private function isLuckyV2($n)
     {
-        $result = array_sum(str_split(substr($n, 0, strlen($n) / 2)))
-            == array_sum(str_split(substr($n, strlen($n) / 2)));
+        $a = array_sum(str_split(substr($n, 0, strlen($n) / 2)));
+        $b = array_sum(str_split(substr($n, strlen($n) / 2)));
 
-        return $result;
+        return $a === $b;
     }
 
     public static function getInput()
