a53f469c8e974979b218f8a4dfca4180bb6aaa04
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-03-02 19:03:53 -0300

    Century From Year.

--- arcade-mode/102- centuryFromYear/solution.php
+++ arcade-mode/102- centuryFromYear/solution.php
@@ -3,7 +3,7 @@
 function centuryFromYear($year)
 {
     $end = substr($year, -2, 2);
-    if ($end == '00') {
+    if ($end === '00') {
         return (int) floor($year / 100);
     } else {
         return (int) floor($year / 100) + 1;
