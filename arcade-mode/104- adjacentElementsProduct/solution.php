8ff90cd70e3dbd4f201a938d774a6ae0e7ebc7d1
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-03-02 19:07:31 -0300

    Adjacent Elements Product.

--- arcade-mode/104- adjacentElementsProduct/solution.php
+++ arcade-mode/104- adjacentElementsProduct/solution.php
@@ -4,8 +4,9 @@ function adjacentElementsProduct($inputArray)
 {
     $max = $inputArray[0] * $inputArray[1];
     for ($i = 1; $i < count($inputArray); $i++) {
-        $total = $inputArray[$i-1] * $inputArray[$i];
+        $total = $inputArray[$i] * $inputArray[$i-1];
         $max = max($max, $total);
     }
+
     return $max;
 }
