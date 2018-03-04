63fbf5b816707da282a5d74949f4b381e3a7efc9
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-03-04 18:03:56 -0300

    Almost Increasing Sequence.

--- arcade-mode/107- almostIncreasingSequence/tests.php
+++ arcade-mode/107- almostIncreasingSequence/tests.php
@@ -4,23 +4,43 @@
 require_once 'solution.php';
 
 $input = [
-    [6, 2, 3, 8],
-    [0, 3],
-    [5, 4, 6],
-    [6, 3],
-    [1],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 5, 9],
+    [1, 3, 2, 1],
+    [1, 3, 2],
+    [1, 2, 1, 2],
+    [1, 4, 10, 4, 2],
+    [10, 1, 2, 3, 4, 5],
+    [1, 1, 1, 2, 3],
+    [0, -2, 5, 6],
+    [1, 2, 3, 4, 5, 3, 5, 6],
+    [40, 50, 60, 10, 20, 30],
+    [1, 1],
+    [1, 2, 5, 3, 5],
+    [1, 2, 5, 5, 5],
+    [10, 1, 2, 3, 4, 5, 6, 1],
+    [1, 2, 3, 4, 3, 6],
+    [1, 2, 3, 4, 99, 5, 6],
+    [123, -17, -5, 1, 2, 3, 12, 43, 45],
+    [3, 5, 67, 98, 3],
 ];
 
 $output = [
-    3,
-    2,
-    0,
-    2,
-    0,
-    0,
-    6,
+    false,
+    true,
+    false,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
 ];
 
 $solutions = [];
@@ -28,7 +48,7 @@ $solutions = [];
 $i = 1;
 echo "TESTS: \n";
 foreach ($input as $t => $test) {
-    $result = makeArrayConsecutive2($test);
+    $result = almostIncreasingSequence($test);
     if ($result === $output[$t]) {
         echo "$i- OK.";
     } else {
