d09eabede6157a0078a620605271ad9ae0ceefe1
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-03-19 00:57:07 -0300

    Common Character Count.

--- arcade-mode/110- commonCharacterCount/solution.php
+++ arcade-mode/110- commonCharacterCount/solution.php
@@ -1,16 +1,16 @@
 <?php
 
 function commonCharacterCount($s1, $s2) {
-    $s1 = str_split($s1);
-    $s2 = str_split($s2);
-    $c = 0;
-    foreach($s1 as $letter) {
-        $key2 = array_search($letter, $s2);
+    $total = 0;
+    $letters1 = str_split($s1);
+    $letters2 = str_split($s2);
+    foreach($letters1 as $letter) {
+        $key2 = array_search($letter, $letters2);
         if($key2 !== false) {
-            unset($s2[$key2]);
-            $c++;
+            unset($letters2[$key2]);
+            $total++;
         }
     }
 
-    return $c;
+    return $total;
 }
