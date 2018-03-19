4a4fa4743431845e03ebc502815dac5d75b464cb
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-03-19 01:06:48 -0300

    Is Lucky.

--- arcade-mode/111- isLucky/solution.php
+++ arcade-mode/111- isLucky/solution.php
@@ -1,57 +1,14 @@
 <?php
 
-class Solution
+function isLucky($n) {
+    return array_sum(str_split(substr($n, 0, strlen($n) / 2))) 
+        == array_sum(str_split(substr($n, strlen($n) / 2)));
+}
+
+function isLuckyV2($n)
 {
-    public function run($x)
-    {
-        return $this->isLuckyV2($x);
-    }
+    $a = array_sum(str_split(substr($n, 0, strlen($n) / 2)));
+    $b = array_sum(str_split(substr($n, strlen($n) / 2)));
 
-    private function isLucky($n)
-    {
-        return array_sum(str_split(substr($n, 0, strlen($n) / 2)))
-            == array_sum(str_split(substr($n, strlen($n) / 2)));
-    }
-
-    private function isLuckyV2($n)
-    {
-        $a = array_sum(str_split(substr($n, 0, strlen($n) / 2)));
-        $b = array_sum(str_split(substr($n, strlen($n) / 2)));
-
-        return $a === $b;
-    }
-
-    public static function getInput()
-    {
-        return [
-            1230,
-            239017,
-            134008,
-            10,
-            11,
-            1010,
-            261534,
-            100000,
-            999999,
-            123321,
-            123456,
-        ];
-    }
-
-    public static function getOutput()
-    {
-        return [
-            true,
-            false,
-            true,
-            false,
-            true,
-            true,
-            false,
-            false,
-            true,
-            true,
-            false,
-        ];
-    }
+    return $a === $b;
 }
--- arcade-mode/111- isLucky/tests.php
+++ arcade-mode/111- isLucky/tests.php
@@ -3,20 +3,52 @@
 
 require_once 'solution.php';
 
-$solution = new Solution();
+$input = [
+    1230,
+    239017,
+    134008,
+    10,
+    11,
+    1010,
+    261534,
+    100000,
+    999999,
+    123321,
+    123456,
+];
+
+$output = [
+    true,
+    false,
+    true,
+    false,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+    false,
+];
+
 $solutions = [];
-$tests = $solution::getInput();
-$output = $solution::getOutput();
 
 $i = 1;
 echo "TESTS: \n";
-foreach ($tests as $t => $test) {
-    $result = $solution->run($test);
+foreach ($input as $t => $test) {
+    $result = isLuckyV2($test);
     if ($result === $output[$t]) {
-        echo "$i- OK.\n";
+        echo "$i- OK.";
     } else {
-        echo "$i- FAIL.\n";
+        echo "$i- FAIL.";
     }
+    if (isset($argv[1]) && $argv[1] === "-v") {
+        echo "\n";
+        echo "Input: ";
+        var_dump($test);
+        echo "Output: " . var_export($result, true);
+    }
+    echo "\n";
     $solutions[] = $result;
     $i++;
 }
